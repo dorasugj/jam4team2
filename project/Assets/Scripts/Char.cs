4d7d6438ea501d4766cbadaaa83a7f593617e458
setchi <ri_xer@yahoo.co.jp>
2014-10-12 13:50:21 +0900

    登場人物一通り追加

---
+++ project/Assets/Scripts/Char.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Char : MonoBehaviour {
+	private int hitPlayey;
+	private Vector3 prevPos;
+	private Vector3 afterPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetPrevPos(Vector3 prevPos) {
+		this.prevPos = prevPos;
+	}
+
+	public void SetAfterPos(Vector3 afterPos) {
+		this.afterPos = afterPos;
+	}
+
+	public void MoveStart() {
+
+	}
+}
